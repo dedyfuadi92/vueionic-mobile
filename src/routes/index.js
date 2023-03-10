4c559588fe0a63b996026dc6ca2badece0d281bc
dedyfuadi92 <dedyfuadi92@gmail.com>
2023-03-10 09:41:37 +0800

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
 export const routes = [
   { path: '/', component: () => import('@/views/Home.vue'), meta: { title: 'Home', auth: true } },
+  { path: '/login', component: () => import('@/views/Login.vue'), meta: { title: 'Login', auth: true } },
 ]
---
+++ src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="h-screen w-full flex flex-col justify bg-slate-800">
+        <h4>Login Page</h4>
+        <ion-grid>
+            <ion-row>
+                <ion-col>
+                    <ion-list>
+                        <ion-item>
+                            <ion-label position="floating">Email</ion-label>
+                            <ion-input placeholder="account@email.com" type="email" pattern="email"></ion-input>
+                        </ion-item>
+                    </ion-list>
+                </ion-col>
+            </ion-row>
+            <ion-row>
+                <ion-col>
+                    <ion-list>
+                        <ion-item>
+                            <ion-label position="floating">Password</ion-label>
+                            <ion-input placeholder="********" type="password" pattern="password"></ion-input>
+                        </ion-item>
+                    </ion-list>
+                </ion-col>
+            </ion-row>
+        </ion-grid>
+    </div>
+</template>
+<script lang="ts">
+import { IonCol, IonGrid, IonRow, IonInput, IonItem, IonLabel, IonList } from '@ionic/vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+    components: { IonCol, IonGrid, IonRow, IonInput, IonItem, IonLabel, IonList },
+});
+</script>
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /* margin-top: 60px; */
+}
+</style>
