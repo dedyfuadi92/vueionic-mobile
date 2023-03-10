3b57fff9f10994897e13a859f1c0f548e987f28e
dedyfuadi92 <dedyfuadi92@gmail.com>
2023-03-10 16:02:03 +0800

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 export const routes = [
   { path: '/', component: () => import('@/views/Home.vue'), meta: { title: 'Home', auth: true } },
   { path: '/login', component: () => import('@/views/Login.vue'), meta: { title: 'Login', auth: true } },
+  { path: '/todo', component: () => import('@/views/Todo.vue'), meta: { title: 'Todo', auth: true } },
 ]
---
+++ src/views/Todo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="w-full h-screen mx-auto bg-slate-800 text-white flex justify-center items-start px-6 pt-4">
+        <div class="w-full">
+            <div class="w-full flex justify-center">
+                <h3>
+                    <strong>
+                        Todo Application
+                    </strong>
+                </h3>
+            </div>
+            <input type="text" name="todoItem" id="todoItem" class="w-full text-lg text-violet-800 p-2 rounded-xl"
+                maxlength="20" minlength="1" required placeholder="let's doing something great...">
+            <div class="h-3"></div>
+            <button type="button" class="w-full bg-gradient-to-r from-violet-500 to-fuchsia-500 p-2 rounded-xl text-lg">
+                <strong>
+                    Add This Todo !
+                </strong>
+            </button>
+            <div class="h-5"></div>
+            <div class="w-full flex justify-center">
+                <div class="w-1/2 h-1 bg-fuchsia-500 rounded-lg">
+                </div>
+            </div>
+            <div class="h-3"></div>
+            <div class="w-full flex justify-center">
+                <h3>
+                    <strong>
+                        Todo List Below
+                    </strong>
+                </h3>
+            </div>
+            <div class="w-full text-lg border-dashed border-2 p-2 rounded-lg text-white">
+                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                <label for="vehicle1"> Playing MSFS</label><br>
+            </div>
+            <div class="h-2"></div>
+        </div>
+    </div>
+</template>
+<script setup></script>
