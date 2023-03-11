551508c4779939b2caa5ac7ad01e1faeb7e8f680
dedyfuadi92 <dedyfuadi92@gmail.com>
2023-03-11 09:53:45 +0800

    fix 1

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -39,22 +39,24 @@
             </div>
 
             <!-- mulai perulangan -->
-            <div v-for="todo in todos" :key="todo.id" >
+            <div v-for="todo in todos" :key="todo.id">
                 <div class="flex flex-row">
-                    <div class="basis-5/6 h-12 text-lg border-solid border-violet-500 border-2 p-2 rounded-lg text-white">
+                    <div @click="$event => doneTodo(todo.id)"
+                        class="basis-5/6 h-12 text-lg border-solid border-violet-500 border-b-2 border-l-2 border-t-2  p-2 rounded-tl-lg rounded-bl-lg text-white">
                         <!-- <input type="checkbox" id="item{{ todo.id }}" name="item{{ todo.id }}" value="item{{ todo.id }}"> -->
-                        <button type="button" class="bg-white p-1 h-full w-6 float-left text-violet-500 rounded-md"
-                            @click="removeTodo(todo)">
-                            <strong>v</strong>
-                        </button>
-                        <label for="item{{ todo.id }}">
-                            &nbsp;
+                        <input type="text"
+                            class="bg-white p-1 h-full w-7 float-left text-violet-500 rounded-md text-center font-bold"
+                            v-if="todo.doneYet" value="v" disabled>
+                        <input type="text"
+                            class="bg-white p-1 h-full w-7 float-left text-violet-500 rounded-md text-center font-bold"
+                            v-else disabled>
+                        <label for="item{{ todo.id }}" :class="{ doneYet: todo.doneYet }" class="pl-4">
                             {{ todo.text }}
                         </label>
                     </div>
-                    <div class="basis-1/6 h-12 text-lg pl-1 rounded-lg text-white">
-                        <button type="button" class="bg-fuchsia-800 p-1 h-full w-full float-right text-white rounded-md"
-                            @click="removeTodo(todo)">
+                    <div
+                        class="basis-1/6 h-12 bg-fuchsia-800 text-lg border-solid border-violet-500 border-t-2 border-r-2 border-b-2 text-white rounded-tr-lg rounded-br-lg">
+                        <button type="button" class="h-full w-full float-right" @click="removeTodo(todo)">
                             <strong>X</strong>
                         </button>
                     </div>
@@ -70,16 +72,25 @@
 <script setup>
 import { ref } from 'vue';
 let id = 0
+// let doneYet = ''
 const newTodo = ref('')
 const todos = ref([])
 function addTodo() {
     if (newTodo.value != '') {
-        todos.value.push({ id: id++, text: newTodo.value })
+        todos.value.push({ id: id++, text: newTodo.value, doneYet: false })
         newTodo.value = ''
     }
 }
-
+function doneTodo(id) {
+    // console.log(id, todos.value[id].text);
+    todos.value[id].doneYet = !todos.value[id].doneYet
+}
 function removeTodo(todo) {
     todos.value = todos.value.filter((t) => t !== todo)
 }
 </script>
+<style scoped>
+.doneYet {
+    text-decoration: line-through;
+}
+</style>
